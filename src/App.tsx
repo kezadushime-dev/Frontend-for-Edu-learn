5e5f4c27c1085d14d98c96508cbdaf2cb7ac4154
keza kevin <kezadushime@gmail.com>
2026-02-11 16:14:19 +0200

    fixed the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 import DashboardAdmin from './pages/DashboardAdmin';
 import DashboardLearner from './pages/DashboardLearner';
-// import DashboardLearnerIntegrated from './pages/DashboardInstructor';
 import DashboardInstructor from './pages/DashboardInstructor';
+import DashboardManager from './pages/DashboardManager';
 import Lesson from './pages/Lesson';
 import Quiz from './pages/Quiz';
 import AdminUsers from './pages/AdminUsers';
@@ -16,6 +16,10 @@ import AdminQuizzes from './pages/AdminQuizzes';
 import AdminQuizAttempts from './pages/AdminQuizAttempts';
 import LessonCreate from './pages/LessonCreate';
 import QuizCreate from './pages/QuizCreate';
+import InstructorLessons from './pages/InstructorLessons';
+import InstructorLessonCreate from './pages/InstructorLessonCreate';
+import InstructorQuizzes from './pages/InstructorQuizzes';
+import InstructorQuizCreate from './pages/InstructorQuizCreate';
 import { useLucide } from './utils/useLucide';
 
 
@@ -34,6 +38,7 @@ function App() {
       <Route path="dashboard-admin" element={<DashboardAdmin />} />
       <Route path="dashboard-learner" element={<DashboardLearner />} />
       <Route path="dashboard-instructor" element={<DashboardInstructor />} />
+      <Route path="dashboard-manager" element={<DashboardManager />} />
 
       <Route path="lesson" element={<Lesson />} />
       <Route path="quiz" element={<Quiz />} />
@@ -45,6 +50,10 @@ function App() {
 
       <Route path="lesson-create" element={<LessonCreate />} />
       <Route path="quiz-create" element={<QuizCreate />} />
+      <Route path="instructor/lessons" element={<InstructorLessons />} />
+      <Route path="instructor/lesson-create" element={<InstructorLessonCreate />} />
+      <Route path="instructor/quizzes" element={<InstructorQuizzes />} />
+      <Route path="instructor/quiz-create" element={<InstructorQuizCreate />} />
  
       
 
